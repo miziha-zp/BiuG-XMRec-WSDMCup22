17aed41a0fe888fdd552b86a6fa8cd1876dfd617
FizzerYu <36132150+FizzerYu@users.noreply.github.com>
2022-02-11 02:23:49 +0800

    delete output

--- ultraGCN/run.sh
+++ ultraGCN/run.sh
@@ -1,4 +1,4 @@
-python solve.py --dir="../input/t1" --config-file="t1.ini" > long.log
-python solve.py --dir="../input/t2" --config-file="t2.ini" >> long.log
-python solve.py --dir="../input/t1" --config-file="t1_testing.ini" >> long.log
-python solve.py --dir="../input/t2" --config-file="t2_testing.ini" >> long.log
+python solve.py --dir="../input/t1" --config-file="t1.ini"
+python solve.py --dir="../input/t2" --config-file="t2.ini"
+python solve.py --dir="../input/t1" --config-file="t1_testing.ini"
+python solve.py --dir="../input/t2" --config-file="t2_testing.ini"
